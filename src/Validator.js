723d1c1e625e119ff79195a38b2135830bbdedf3
suchethaleonidou <suchethaleonidou>
2023-01-16 15:20:31 -0500

    commit 2

--- src/Validator.js
+++ src/Validator.js
@@ -1,42 +1,105 @@
 import React, { Component, useState } from 'react';
 import './Validator.css';
 
-  function Validator() {
- 
-    const [input, setInput] = useState({
-      email: '',
-      password: '',
-      confirmPassword: ''
-    });
-   
-    const [error, setError] = useState({
-      email: '',
-      password: '',
-      confirmPassword: ''
-    })
-   
-    const onInputChange = e => {
-   
-    }
-   
-    const validateInput = e => {
-   
-    }
-  
-    return (
-      <div className="form">
-        <h1>Sign Up</h1>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="passwordConfirm" placeholder="confirm password" />
-        <input 
-        type="button" 
-        value="Submit!"
-        //add onClick={this.handleClick}
-         />
-      </div>
-    );
+function Validator() {
+
+  const [input, setInput] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const [error, setError] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const onInputChange = e => {
+    const { name, value } = e.target;
+    setInput(prev => ({
+      ...prev,
+      [name]: value
+    }));
+    validateInput(e);
   }
 
+  const validateInput = e => {
+    let { name, value } = e.target;
+    setError(prev => {
+      const stateObj = { ...prev, [name]: "" };
+
+      switch (name) {
+        case "username":
+          if (!value) {
+            stateObj[name] = "Please enter Username.";
+          }
+          break;
+
+        case "password":
+          if (!value) {
+            stateObj[name] = "Please enter Password.";
+          } else if (input.confirmPassword && value !== input.confirmPassword) {
+            stateObj["confirmPassword"] = "Password and Confirm Password does not match.";
+          } else {
+            stateObj["confirmPassword"] = input.confirmPassword ? "" : error.confirmPassword;
+          }
+          break;
+
+        case "confirmPassword":
+          if (!value) {
+            stateObj[name] = "Please enter Confirm Password.";
+          } else if (input.password && value !== input.password) {
+            stateObj[name] = "Password and Confirm Password does not match.";
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      return stateObj;
+    });
+  }
+
+  return (
+    <div className="app">
+      <h1>Sign Up</h1>
+      <form>
+
+        <input
+          type="text"
+          name="username"
+          placeholder='username'
+          value={input.username}
+          onChange={onInputChange}
+          onBlur={validateInput}></input>
+        {error.username && <span className='err'>{error.username}</span>}
+
+        <input
+          type="password"
+          name="password"
+          placeholder='password'
+          value={input.password}
+          onChange={onInputChange}
+          onBlur={validateInput}></input>
+        {error.password && <span className='err'>{error.password}</span>}
+
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder='confirm Password'
+          value={input.confirmPassword}
+          onChange={onInputChange}
+          onBlur={validateInput}></input>
+        {error.confirmPassword && <span className='err'>{error.confirmPassword}</span>}
+
+        <button>Submit</button>
+
+        <h4>Valid!</h4>
+      </form>
+    </div>
+  );
+}
 
 export default Validator;
