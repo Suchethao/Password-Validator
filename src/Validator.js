be7555c48d05cd4997ce56887770a06a7befbea7
suchethaleonidou <suchethaleonidou>
2023-01-16 14:53:25 -0500

    first commit

--- src/Validator.js
+++ src/Validator.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './Validator.css';
 
-class Validator extends Component {
-  render() {
+  function Validator() {
+ 
+    const [input, setInput] = useState({
+      email: '',
+      password: '',
+      confirmPassword: ''
+    });
+   
+    const [error, setError] = useState({
+      email: '',
+      password: '',
+      confirmPassword: ''
+    })
+   
+    const onInputChange = e => {
+   
+    }
+   
+    const validateInput = e => {
+   
+    }
+  
     return (
       <div className="form">
         <h1>Sign Up</h1>
-        <input type="text" placeholder="username" />
+        <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
+        <input type="passwordConfirm" placeholder="confirm password" />
+        <input 
+        type="button" 
+        value="Submit!"
+        //add onClick={this.handleClick}
+         />
       </div>
     );
   }
-}
+
 
 export default Validator;
